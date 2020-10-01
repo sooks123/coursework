824695b6d9c3ed28aa0beff7519337926a0a8fb5
simonojok19 <simonojok19@gmail.com>
2020-10-01 18:21:52 +0300

    started work

---
+++ simon.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Soldier
+{
+    struct Soldier *prev;
+    int sequence;
+    struct Soldier *next;
+};
+
+struct Queue
+{
+    int nodeCount;
+    int k;
+    int th;
+    char name[50];
+    struct Soldier *front;
+    struct Soldier *back;
+};
+
+// Functions
+void enqueue();
+void dequeue();
+void peek();
+int isEmpty();
+
+int main()
+{
+    return 0;
+}
